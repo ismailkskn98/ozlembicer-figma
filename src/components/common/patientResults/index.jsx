3b500f1e4f0748043d7558deccef9309d7ad3662
İsmail Keskin <hamdismailkskn98@gmail.com>
2026-02-04 15:39:17 +0300

    feat: PatientResults ve PatientResultCarousel bileşenlerinde stil güncellemeleri yapıldı; responsive tasarım iyileştirildi.

--- src/components/common/patientResults/index.jsx
+++ src/components/common/patientResults/index.jsx
@@ -5,8 +5,8 @@ import MotionScrollInView from '../motionScrollInView'
 export default function PatientResults() {
     return (
         <MotionScrollInView className='w-full py-10'>
-            <main className='w-full max-w-full xl:max-w-6xl mx-auto flex flex-col items-center justify-center gap-7'>
-                <h2 className='text-3xl font-semibold text-center'>Patient Results</h2>
+            <main className='w-full max-w-full xl:max-w-6xl mx-auto flex flex-col items-center justify-center gap-5 sm:gap-7'>
+                <h2 className='text-[20px] lg:text-3xl font-semibold text-center'>Patient Results</h2>
                 <PatientResultCarousel />
             </main>
         </MotionScrollInView>
--- src/components/common/patientResults/patientResultCarousel.jsx
+++ src/components/common/patientResults/patientResultCarousel.jsx
@@ -55,20 +55,30 @@ const carouselItems = [
 
 export default function PatientResultCarousel() {
     return (
-        <div className='w-full flex flex-col items-center justify-center gap-8'>
+        <div className='w-full flex flex-col items-center justify-center gap-y-6 sm:gap-y-7'>
             <Swiper
                 modules={[Navigation, Autoplay]}
-                spaceBetween={50}
                 slidesPerView={3}
                 breakpoints={{
+                    1280: {
+                        slidesPerView: 3,
+                        spaceBetween: 50
+                    },
                     900: {
                         slidesPerView: 3,
+                        spaceBetween: 30
+                    },
+                    740: {
+                        slidesPerView: 2.5,
+                        spaceBetween: 20
                     },
                     500: {
                         slidesPerView: 2,
+                        spaceBetween: 20
                     },
                     100: {
-                        slidesPerView: 1,
+                        slidesPerView: 1.5,
+                        spaceBetween: 20
                     },
                 }}
                 centeredSlides={true}
@@ -82,33 +92,39 @@ export default function PatientResultCarousel() {
                 className='w-full'
             >
                 {carouselItems.map((item, index) => (
-                    <SwiperSlide className='bg-linear-to-b from-stone-beige to-ivory-soft p-2.5 rounded-[10px]'>
+                    <SwiperSlide className='' key={index}>
                         {({ isActive }) => (
-                            <div className={`flex flex-col items-center justify-center w-full gap-5 rounded-[10px] ${!isActive && 'blur-[2px]'}`}>
-                                <div className='flex items-center gap-1.25'>
-                                    <Image src={item.beforeImage} alt={`Patient before ${index + 1}`} width={300} height={250} className='w-70 h-auto object-cover object-center rounded-[5px]' />
-                                    <Image src={item.afterImage} alt={`Patient after ${index + 1}`} width={300} height={250} className='w-70 h-auto object-cover object-center rounded-[5px]' />
+                            <section className='bg-linear-to-b from-stone-beige to-ivory-soft p-2.5 rounded-[10px]'>
+                                <div className={`w-full flex flex-col items-center justify-center gap-2.5 rounded-[10px] ${!isActive && 'blur-[2px]'}`}>
+                                    <div className='w-full flex items-center gap-1.25'>
+                                        <div className='relative w-full h-70 rounded-[5px] overflow-hidden'>
+                                            <Image src={item.beforeImage} alt={`Patient before ${index + 1}`} fill className='w-full h-full object-cover object-center rounded-[5px]' />
+                                        </div>
+                                        <div className='relative w-full h-70 rounded-[5px] overflow-hidden'>
+                                            <Image src={item.afterImage} alt={`Patient after ${index + 1}`} width={300} height={250} className='w-full h-70 object-cover object-center rounded-[5px]' />
+                                        </div>
+                                    </div>
+                                    <article className='w-full grid grid-cols-3 place-content-stretch justify-items-stretch bg-coffee-dark text-ivory-soft px-2.5 rounded-[5px] h-15'>
+                                        <div className='relative flex flex-col items-center justify-center text-center gap-0.5'>
+                                            <h5 className='font-bold text-[10px] sm:text-[12px]'>TECHNIQUE</h5>
+                                            <Image src='/images/heroCard-line.png' alt='line' width={60} height={5} className='w-12.75 h-fit object-contain object-center' />
+                                            <p className='text-[8px] sm:text-[10px]'>{item.technique}</p>
+                                            <Image src='/images/heroCard-line.png' alt='line' width={50} height={5} className='absolute top-1/2 -translate-y-1/2 left-full -translate-x-1/2 -rotate-90 object-contain object-center w-10.5 h-4' />
+                                        </div>
+                                        <div className='relative flex flex-col items-center justify-center text-center gap-0.5'>
+                                            <h5 className='font-bold text-[10px] sm:text-[12px]'>GRAFTS</h5>
+                                            <Image src='/images/heroCard-line.png' alt='line' width={60} height={5} className='w-12.75 h-fit object-contain object-center' />
+                                            <p className='text-[8px] sm:text-[10px]'>{item.grafts}</p>
+                                            <Image src='/images/heroCard-line.png' alt='line' width={50} height={5} className='absolute top-1/2 -translate-y-1/2 left-full -translate-x-1/2 -rotate-90 object-contain object-center w-10.5 h-4' />
+                                        </div>
+                                        <div className='relative flex flex-col items-center justify-center text-center gap-0.5'>
+                                            <h5 className='font-bold text-[10px] sm:text-[12px]'>AFTER</h5>
+                                            <Image src='/images/heroCard-line.png' alt='line' width={60} height={5} className='w-12.75 h-fit object-contain object-center' />
+                                            <p className='text-[8px] sm:text-[10px]'>{item.afterMonths}</p>
+                                        </div>
+                                    </article>
                                 </div>
-                                <article className='w-full grid grid-cols-3 place-content-stretch justify-items-stretch bg-coffee-dark text-ivory-soft px-2.5 rounded-[5px] h-15'>
-                                    <div className='relative flex flex-col items-center justify-center text-center gap-0.5'>
-                                        <h5 className='font-bold text-[8px]'>TECHNIQUE</h5>
-                                        <Image src='/images/heroCard-line.png' alt='line' width={60} height={5} className='w-12.75 h-fit object-contain object-center' />
-                                        <p className='text-[8px]'>{item.technique}</p>
-                                        <Image src='/images/heroCard-line.png' alt='line' width={50} height={5} className='absolute top-1/2 -translate-y-1/2 left-full -translate-x-1/2 -rotate-90 object-contain object-center w-10.5 h-4' />
-                                    </div>
-                                    <div className='relative flex flex-col items-center justify-center text-center gap-0.5'>
-                                        <h5 className='font-bold text-[8px]'>GRAFTS</h5>
-                                        <Image src='/images/heroCard-line.png' alt='line' width={60} height={5} className='w-12.75 h-fit object-contain object-center' />
-                                        <p className='text-[8px]'>{item.grafts}</p>
-                                        <Image src='/images/heroCard-line.png' alt='line' width={50} height={5} className='absolute top-1/2 -translate-y-1/2 left-full -translate-x-1/2 -rotate-90 object-contain object-center w-10.5 h-4' />
-                                    </div>
-                                    <div className='relative flex flex-col items-center justify-center text-center gap-0.5'>
-                                        <h5 className='font-bold text-[8px]'>AFTER</h5>
-                                        <Image src='/images/heroCard-line.png' alt='line' width={60} height={5} className='w-12.75 h-fit object-contain object-center' />
-                                        <p className='text-[8px]'>{item.afterMonths}</p>
-                                    </div>
-                                </article>
-                            </div>
+                            </section>
                         )}
                     </SwiperSlide>
                 ))}
