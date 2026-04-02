d20df8e7989a6227f3b38a7f4f51a8032a865ddc
İsmail Keskin <hamdismailkskn98@gmail.com>
2026-04-02 11:50:44 +0300

    feat: Yeni ikon dosyaları eklendi (body-icon, consultation-icon, eyebrow-icon, prp-icon, scalp-icon)

--- src/components/about/pricing/index.jsx
+++ src/components/about/pricing/index.jsx
@@ -1,31 +1,31 @@
-import CustomBreadCrumb from '@/components/common/customBreadCrumb'
-import HowToAppointment from '@/components/common/howToAppointment'
-import HeroLogoCarousel from '@/components/common/logoCarousel'
-import React from 'react'
-import PatientResults from '@/components/common/patientResults'
-import PricingHero from './pricingHero'
-import TitleImages from '@/components/common/titleImages'
+import CustomBreadCrumb from '@/components/common/customBreadCrumb';
+import HowToAppointment from '@/components/common/howToAppointment';
+import HeroLogoCarousel from '@/components/common/logoCarousel';
+import React from 'react';
+import PatientResults from '@/components/common/patientResults';
+import PricingHero from './pricingHero';
+import DeneyTitleImage from '@/components/common/titleImages/deney';
 
 const linksItems = [
-    { label: 'About Dr. Ozlem Bicer', href: '/about-us/dr-ozlem-bicer' },
-    { label: 'Clinic & Team', href: '/about-us/clinic-team' },
-    { label: 'Pricing', href: '#' },
-]
+   { label: 'About Dr. Ozlem Bicer', href: '/about-us/dr-ozlem-bicer' },
+   { label: 'Clinic & Team', href: '/about-us/clinic-team' },
+   { label: 'Pricing', href: '#' },
+];
 
 export default function PricingMain({ children }) {
-    return (
-        <main className='w-full fluid gridContainer bg-ivory-soft'>
-            <CustomBreadCrumb links={linksItems} />
-            <div className='fluid gridContainer w-full bg-linear-to-b from-stone-beige via-ivory-soft to-stone-beige/50'>
-                <PricingHero />
-                <HeroLogoCarousel />
-                {children}
-                <TitleImages />
-                <PatientResults title="Patient Results" />
-                <div className='-mt-10 lg:mt-0'>
-                    <HowToAppointment />
-                </div>
+   return (
+      <main className="w-full fluid gridContainer bg-ivory-soft">
+         <CustomBreadCrumb links={linksItems} />
+         <div className="fluid gridContainer w-full bg-linear-to-b from-stone-beige via-ivory-soft to-stone-beige/50">
+            <PricingHero />
+            <HeroLogoCarousel />
+            {children}
+            <DeneyTitleImage />
+            <PatientResults title="Patient Results" />
+            <div className="-mt-10 lg:mt-0">
+               <HowToAppointment />
             </div>
-        </main>
-    )
+         </div>
+      </main>
+   );
 }
