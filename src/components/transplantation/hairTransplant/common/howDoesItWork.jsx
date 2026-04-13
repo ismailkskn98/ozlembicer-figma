3121cd7bb8703048941f60068a81cc20336a8fde
İsmail Keskin <hamdismailkskn98@gmail.com>
2026-04-13 09:24:54 +0300

    feat: HowDoesItWork bileşenine mobil carousel eklendi; video ve resimlerin gösterimi güncellendi.

--- src/components/transplantation/hairTransplant/common/howDoesItWork.jsx
+++ src/components/transplantation/hairTransplant/common/howDoesItWork.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Image from 'next/image';
 import { cn } from '@/lib/utils';
 import { IoMdPlay } from 'react-icons/io';
+import HowDoesItWorksMobilCarousel from './howDoesItWorksMobilCarousel';
 
 export default function HowDoesItWork({ images = [], videos = [], title = [], content = [], children, className }) {
    const RecursiveList = ({ items }) => {
@@ -20,10 +21,10 @@ export default function HowDoesItWork({ images = [], videos = [], title = [], co
       <main className={cn('w-full max-w-6xl mx-auto gap-6 grid grid-cols-1 min-[900px]:grid-cols-3 text-coffee-dark', className)}>
          <section className="hidden min-[900px]:flex flex-col items-start gap-2.5 h-full">
             {videos.length > 0
-               ? videos.map((video, index) => <video key={index} src={video.src} width={600} height={800} className="object-cover h-65 lg:h-70 w-full" autoPlay muted loop playsInline />)
+               ? videos.map((video, index) => <video key={index} src={video.src} width={600} height={800} className="object-cover aspect-368/280" autoPlay muted loop playsInline />)
                : images.map((image, index) => (
                     <div key={index} className="relative w-fit h-fit overflow-hidden">
-                       <Image src={image.src} alt={image.alt} width={600} height={800} className="object-cover h-65 lg:h-70 w-full" />
+                       <Image src={image.src} alt={image.alt} width={600} height={800} className="object-cover aspect-368/280" />
                        <div className="absolute left-1/2 top-1/2 -translate-1/2 bg-white rounded-full w-14 h-14 flex items-center justify-center pl-1 border border-gray-400">
                           <IoMdPlay className="text-[#ff5524] text-2xl" />
                        </div>
@@ -64,6 +65,7 @@ export default function HowDoesItWork({ images = [], videos = [], title = [], co
             )}
             {children}
          </section>
+         <HowDoesItWorksMobilCarousel />
       </main>
    );
 }
---
+++ src/components/transplantation/hairTransplant/common/howDoesItWorksMobilCarousel.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function HowDoesItWorksMobilCarousel({ images = [], videos = [] }) {
+   return <section className="min-[900px]:hidden">HowDoesItWorksMobilCarousel</section>;
+}
