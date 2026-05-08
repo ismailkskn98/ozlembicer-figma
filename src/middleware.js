98228a670135a673eb51d7cb0b56015f321dc21f
İsmail Keskin <hamdismailkskn98@gmail.com>
2026-05-08 14:17:09 +0300

    fix: routing import yolu düzeltildi

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,5 @@
 import createMiddleware from 'next-intl/middleware';
-import { routing } from './i18n/routing';
+import { routing } from './src/i18n/routing';
 import { NextResponse } from 'next/server';
 
 const SITE_URL = 'https://ozlembicer.com';
